6d382f2e3d3d7f85f44d1b1cbf48ff1231eaf927
WFacuri <winistonfacuri@gmail.com>
2025-08-22 17:28:17 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FleetManager from './components/FleetManager';
+
+function App() {
+  return (
+    <div className="App">
+      <FleetManager />
+    </div>
+  );
+}
+
+export default App;
