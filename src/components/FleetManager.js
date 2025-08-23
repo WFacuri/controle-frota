dc351463b2870278a73955262cbef6913ff2a619
WFacuri <winistonfacuri@gmail.com>
2025-08-23 13:34:55 -0300

    Update FleetManager.js

--- src/components/FleetManager.js
+++ src/components/FleetManager.js
@@ -1,3 +1,5 @@
+/* global firebase */
+
 // Forçando a atualização para a Vercel - v3
 import React, { useState, useEffect } from 'react';
 import { Truck, Fuel, Wrench, MapPin, BarChart3, Edit, Trash2, Save, X, LogOut, User, Lock } from 'lucide-react';
